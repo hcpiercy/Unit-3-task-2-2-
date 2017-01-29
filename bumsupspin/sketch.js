421a9f9d407d9d3d6f4958d443ef0afc7aa0c065
hcpiercy <harrietzw@gmail.com>
2017-01-29 12:49:40 +0000

    Add files via upload

---
+++ bumsupspin/sketch.js
@@ -0,0 +1,15 @@
+function preload() {
+  bumsup = loadImage('BUMSUPFINALLOGO.png');
+}
+
+function setup() {
+  frameRate(20);
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  image(bumsup, mouseX, mouseY,
+        200+sin(frameCount*0.1)*100,
+        200+cos(frameCount*0.1)*100);
+}
